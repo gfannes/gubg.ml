021a914dd73d1b44100a2bbb4d98b8613020803d
Geert Fannes <geert.fannes@gmail.com>
2018-06-19 23:43:40 +0200

    wip

--- src/gubg/neural/mlp.py
+++ src/gubg/neural/mlp.py
@@ -1,10 +1,16 @@
+class Neuron:
+    def __init__(self, input_ixs, weight_offset):
+        self.input_ixs = input_ixs
+        self.weight_offset = weight_offset
+
 class Layer:
-    def __init__(self, transfer, nr_inputs, nr_outputs):
+    def __init__(self, transfer, nr_inputs, nr_neurons):
         self.transfer = transfer
         self.nr_inputs = nr_inputs
-        self.nr_outputs = nr_outputs
+        self.nr_neurons = nr_neurons
+        self.neurons = []
     def __str__(self):
-        return "[layer](inputs:{inp})(outputs:{outp})(transfer:{tf})".format(tf=self.transfer, inp=self.nr_inputs, outp=self.nr_outputs)
+        return "[layer](inputs:{inp})(outputs:{outp})(transfer:{tf})".format(tf=self.transfer, inp=self.nr_inputs, outp=self.nr_neurons)
 
 class MLP:
     def __init__(self, nr_inputs):
@@ -12,19 +18,21 @@ class MLP:
         self.layers = []
     
     def add_layer(self, transfer, nr_neurons):
-        nr_inputs = self.layers[-1].nr_outputs if self.layers else self.nr_inputs
+        nr_inputs = self.layers[-1].nr_neurons if self.layers else self.nr_inputs
         self.layers.append(Layer(transfer, nr_inputs, nr_neurons))
     def setup_simulator(self, simulator):
         simulator.clear()
 
         self.bias = simulator.add_external(1)
-        self.input0 = simulator.add_external(self.nr_inputs)
+        self.input_offset = simulator.add_external(self.nr_inputs)
 
-        input_ixs = [self.bias]+[i+self.input0 for i in range(self.nr_inputs)]
+        input_ixs = [i+self.input_offset for i in range(self.nr_inputs)]+[self.bias]
 
         for layer in self.layers:
-            new_input_ixs = [self.bias]
-            for i in range(layer.nr_outputs):
-                output_ix, weight_ix = simulator.add_neuron(layer.transfer, input_ixs)
+            new_input_ixs = []
+            for i in range(layer.nr_neurons):
+                output_ix, weight_offset = simulator.add_neuron(layer.transfer, input_ixs)
+                neuron = Neuron(input_ixs, weight_offset)
+                layer.neurons.append(neuron)
                 new_input_ixs.append(output_ix)
-            input_ixs = new_input_ixs
+            input_ixs = new_input_ixs+[self.bias]
