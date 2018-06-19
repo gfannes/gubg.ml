bc4417cbdb6b4f5f246b12bf280db09e18dc0f36
Geert Fannes <geert.fannes@gmail.com>
2018-06-19 18:31:21 +0200

    wip

--- src/gubg/neural/mlp.py
+++ src/gubg/neural/mlp.py
@@ -14,3 +14,17 @@ class MLP:
     def add_layer(self, transfer, nr_neurons):
         nr_inputs = self.layers[-1].nr_outputs if self.layers else self.nr_inputs
         self.layers.append(Layer(transfer, nr_inputs, nr_neurons))
+    def setup_simulator(self, simulator):
+        simulator.clear()
+
+        self.bias = simulator.add_external(1)
+        self.input0 = simulator.add_external(self.nr_inputs)
+
+        input_ixs = [self.bias]+[i+self.input0 for i in range(self.nr_inputs)]
+
+        for layer in self.layers:
+            new_input_ixs = [self.bias]
+            for i in range(layer.nr_outputs):
+                output_ix, weight_ix = simulator.add_neuron(layer.transfer, input_ixs)
+                new_input_ixs.append(output_ix)
+            input_ixs = new_input_ixs
--- src/gubg/neural/simulator.py
+++ src/gubg/neural/simulator.py
@@ -1,15 +1,19 @@
 from . import neuron
 
 class Simulator:
-    def __init__(self):
+    def clear(self):
         self.nr_states, self.nr_weights = 0, 0
         self._neurons = []
+
+    def __init__(self):
+        self.clear()
+    def __str__(self):
+        return "[simulator](states:{states})(weights:{weights})(neurons:{neurons})".format(states=self.nr_states, weights=self.nr_weights, neurons=len(self._neurons))
 
     def add_external(self, nr):
         ix = self.nr_states
         self.nr_states += nr
         return ix
-
     def add_neuron(self, transfer, inputs):
         output_ix, weight_ix = self.nr_states, self.nr_weights
 
