e43827b14a7c27673b1e21a03cda2d23e4518115
Geert Fannes <geert.fannes@gmail.com>
2018-08-12 17:17:13 +0200

    details

--- test/src/gubg/mlp/Parameters_tests.cpp
+++ test/src/gubg/mlp/Parameters_tests.cpp
@@ -14,14 +14,14 @@ TEST_CASE("mlp::Parameters tests", "[ut][mlp][Parameters]")
     params1.setup(mlp);
 
     std::string str1;
-    REQUIRE(s11n::write_object(str1, "params", params1));
+    REQUIRE(s11n::write_object(str1, ":mlp.Parameters", params1));
     std::cout << str1 << std::endl;
 
     mlp::Parameters params2;
-    REQUIRE(s11n::read_object(str1, "params", params2));
+    REQUIRE(s11n::read_object(str1, ":mlp.Parameters", params2));
 
     std::string str2;
-    REQUIRE(s11n::write_object(str2, "params", params2));
+    REQUIRE(s11n::write_object(str2, ":mlp.Parameters", params2));
     std::cout << str2 << std::endl;
 
     REQUIRE(str1 == str2);
--- test/src/gubg/mlp/Structure_tests.cpp
+++ test/src/gubg/mlp/Structure_tests.cpp
@@ -11,14 +11,14 @@ TEST_CASE("mlp::Structure tests", "[ut][mlp][Structure]")
     mlp1.add_layer(neural::Transfer::Linear, 1, 0.5, 3.0);
 
     std::string str1;
-    REQUIRE(s11n::write_object(str1, "mlp", mlp1));
+    REQUIRE(s11n::write_object(str1, ":mlp.Structure", mlp1));
     std::cout << C(str1) << std::endl;
 
     mlp::Structure mlp2;
-    REQUIRE(s11n::read_object(str1, "mlp", mlp2));
+    REQUIRE(s11n::read_object(str1, ":mlp.Structure", mlp2));
 
     std::string str2;
-    REQUIRE(s11n::write_object(str2, "mlp", mlp2));
+    REQUIRE(s11n::write_object(str2, ":mlp.Structure", mlp2));
     std::cout << C(str2) << std::endl;
     REQUIRE(str1 == str2);
 }
